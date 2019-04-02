5315541d0884f0fc750556036568655f39775a7f
YentingChen <yeinbo0620@gmail.com>
2019-04-02 14:58:13 +0800

    first commit

---
+++ Goes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goes
+//
+//  Created by Yenting Chen on 2019/4/2.
+//  Copyright © 2019年 yeinting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
